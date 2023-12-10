fa191fcd567b45897152f220640795afc1488781
Quentin Churet <qchuret@gmail.com>
2023-12-10 16:29:01 +0100

    feat: customize secret page

--- src/pages/SecretPage.tsx
+++ src/pages/SecretPage.tsx
@@ -6,9 +6,11 @@ const SecretPage = () => {
     const selectedMember = secretSantaMapping[memberName as keyof typeof secretSantaMapping];
 
     return (
-        <div>
+        <div style={{ padding: "2rem"}}>
             <p>{memberName}, tu dois faire un cadeau à ...</p>
             <h2>{selectedMember} !</h2>
+            <p>N'hésite pas à demander à ses proches si jamais tu n'as pas d'idée ! Mais ne te fais pas découvrir...</p>
+            <p>Une seule règle : ne pas dépasser un montant de 50€ !</p>
             <Link to="/"><button className="button-24">Retour à la page d'accueil</button></Link>
         </div>
     );
